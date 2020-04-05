938710087e5ded26609e93275235b1482635ffd1
Stephan Wells <stephanwells@hotmail.com>
2020-04-05 16:30:01 +0200

    Initial commit and setting up of some API values

---
+++ src/Defs.java
@@ -0,0 +1,12 @@
+public class Defs
+{
+    // API Values
+    public static final String API_JSONRPC = "2.0";
+    public static final String API_ID = "12345";
+    public static final String API_KEY = "6e7aac53b751cc230df25ddf093caea295957081c328b339";
+    public static final String API_SESSION_ID = "c5p3fflrqnqq5g8mocr0jormj4";
+
+    // API Methods
+    public static final String METHOD_GETAPPLICATIONSLIST = "Applications.getList";
+    public static final String METHOD_GETAPPLICATION = "Applications.getApplications";
+}
